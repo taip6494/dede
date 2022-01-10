15e52ba1fa871cf1be3aea8179c97ae93528ca01
taip6494 <palisseauthomas@gmail.com>
2022-01-10 10:34:11 +0400

    Structure de l'application et ajout du row avec les images.asset

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,19 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: EdgeInsets.fromLTRB(25, 20, 25, 0),
+        child: Row(children: [
+          Image.asset(
+            'images/dice1.png',
+            width: 170,
+            height: 170,
+          ),
+          Image.asset(
+            'images/dice2.png',
+            width: 170,
+            height: 170,
+          ),
+        ]));
   }
 }
