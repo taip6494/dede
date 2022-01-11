eb5ee8a6f5b15ac169509e9ca635b8a8accfb3d7
taip6494 <palisseauthomas@gmail.com>
2022-01-11 08:15:46 +0400

    Dédé end

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dede/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( DicePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
